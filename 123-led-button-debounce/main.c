5413a220b15919c5cc6ae77e18d677b74966be03
IRUNKA <irina.o.vysotskaya@gmail.com>
2026-09-16 21:28:46 +0300

    Сделан п1.2.3

---
+++ 123-led-button-debounce/main.c
@@ -0,0 +1,43 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
+
+const uint LED_PIN = 25;
+const uint BUTTON_PIN = 15;
+const uint DEBOUNCE_MS = 20;
+
+bool get_button_debounce(uint pin)
+{
+	bool state = gpio_get(pin);
+	sleep_ms(DEBOUNCE_MS);
+	return state && gpio_get(pin);
+}
+
+int main()
+{
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	gpio_init(BUTTON_PIN);
+	gpio_set_dir(BUTTON_PIN, GPIO_IN);
+	gpio_pull_up(BUTTON_PIN);
+
+	bool led = false;
+	bool previous = false;
+
+	volatile uint32_t *gpio_out_set =
+	  (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
+	volatile uint32_t *gpio_out_clr =
+	  (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
+
+	const uint32_t led_mask = 1u << LED_PIN;
+
+	while (1) {
+		bool current = get_button_debounce(BUTTON_PIN);
+		if (previous == true && current == false) {
+			led = !led;
+			gpio_put(LED_PIN, led);
+		}
+		previous = current;
+	}
+}
